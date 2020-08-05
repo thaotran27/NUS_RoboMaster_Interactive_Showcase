1eacbd157c4e9874271d3e28cc3851285837e69c
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-08-05 11:45:05 +0800

    Prepare for remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+
 import './App.css';
+import "./Animations.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="main">
+        
+        <Switch>
+          <Route path="/" render={() => 
+            <div>
+              <div className="navBar">
+                <img src={require("./imgs/luminus_logo.png")} className="luminusLogo"/>
+                <div className="title">
+                  <h1 align="center">NUS RoboMaster Interactive Showcase</h1>
+                </div>
+                <img src={require("./imgs/nus_engineering_logo.png")} className="nusEnginLogo"/>
+              </div>
+
+              <div className="welcomeContainer">
+                <div className="welcomeBox init_right">
+                  <img src={require("./imgs/rm_logo.png")}  className="rmLogo"/>
+                </div>
+              </div>
+
+              <p id="sizeShow">Hell</p>
+            </div>
+          }/>
+          
+
+        </Switch>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "./Animations.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,6 +11,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+var sizeShow = document.getElementById("sizeShow");
+console.log(sizeShow);
+sizeShow.innerText = "res: " + window.innerWidth + ", " + window.innerHeight;
+
+window.addEventListener("resize", (event) => {
+  sizeShow.innerText = "res: " + window.innerWidth + ", " + window.innerHeight;
+  console.log("size changed");
+});
+
+var animElems = document.getElementsByClassName("startAnim");
+for (var i = 0; i < animElems.length; i++) {
+  animElems[i].classList.add("is_visible");
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
