30117f8c9b526b128f32407f0ec8e9a9ebd41ac1
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-08-10 11:43:29 +0800

    Middle of creating Battle page

--- src/Battle.js
+++ src/Battle.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+
+import { LoginContext } from "./App.js"
 
 import "./Battle.css";
 import "./Animations.css";
 
-class Battle extends Component {
-    constructor() {
-        super();
+function getFreeRobot() {
+
+}
+
+function Battle(props) {
+    if (useContext(LoginContext) === false) {
+        console.log("User not logged in, heading back to home");
+        props.history.push("/");
     }
 
-    render() {
-        return (<h1>Battle Test</h1>);
-    }
+    return (
+        <div className="window-container">
+            <div className="help-container">
+                <h3 align="center">How To Play</h3>
+            </div>
+
+            <div className="game-container">
+                <h3 align="center">Game</h3>
+                <video></video>
+            </div>
+
+            <div className="queue-container">
+                <h3 align="center">Waiting Queue</h3>
+            </div>
+        </div>
+    );
+
 }
 
 export default Battle;
