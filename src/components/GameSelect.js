d81fe0ddac2a2712f2c7854d7aabcf80b8601855
alvynben <alvynben@gmail.com>
2021-08-07 17:45:39 +0800

    Prevent multiple button clicks & add loading

--- src/components/GameSelect.js
+++ src/components/GameSelect.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter, Link, useHistory, useLocation } from "react-router-dom";
 
 import signallingServer from "../api/SignallingServer.js";
@@ -11,6 +11,8 @@ function GameSelect(props) {
   const history = useHistory();
   const location = useLocation();
 
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
   useEffect(() => {
     if (!location.username) {
       window.alert("Please log in before playing a game!");
@@ -19,6 +21,7 @@ function GameSelect(props) {
   }, []);
 
   const goToBattle = () => {
+    setButtonDisabled(true);
     signallingServer
       .findRobot("battle")
       .then((robotName) => {
@@ -33,27 +36,31 @@ function GameSelect(props) {
                 console.log("Game can be started");
                 webRTC.setAnswer(answer);
                 signallingServer.startGame();
+                setButtonDisabled(false);
 
                 history.push({
                   pathname: "/game-select/battle",
                   username: location.username,
-                  purpose: "playing"
+                  purpose: "playing",
                 });
               })
               .catch((error) => {
                 window.alert(error);
+                setButtonDisabled(false);
               });
           })
           .catch((error) => {
             window.alert(error);
+            setButtonDisabled(false);
           });
       })
       .catch((error) => {
         // Handle no robot found
+        setButtonDisabled(false);
         history.push({
           pathname: "/game-select/battle",
           username: location.username,
-          purpose: "waiting"
+          purpose: "waiting",
         });
       });
   };
@@ -65,43 +72,67 @@ function GameSelect(props) {
   return (
     <div className="selection-container init-top">
       <div className="selection-box">
-        <h3 align="center">Please choose a game you would like to play</h3>
+        {buttonDisabled ? (
+          <>
+            <div class="loader"></div>
+            <h3>
+              Loading
+              <span class="loaderdotone">.</span>
+              <span class="loaderdottwo">.</span>
+              <span class="loaderdotthree">.</span>
+            </h3>
+          </>
+        ) : (
+          <h3 align="center">
+            {buttonDisabled
+              ? "Loading"
+              : "Please choose a game you would like to play"}
+          </h3>
+        )}
 
-        <div className="tile-container">
-          <div className="image-container">
-            <div
-              // to="/game-select/battle"
-              onClick={() => goToBattle()}
-            >
-              <img
-                src={require("../assets/battle_image.PNG")}
-                alt="1v1 Battle Selection"
-              />
+        {buttonDisabled ? null : (
+          <div className="tile-container">
+            <div className="image-container">
+              <div
+                // to="/game-select/battle"
+                onClick={() => goToBattle()}
+                style={
+                  buttonDisabled
+                    ? { pointerEvents: "none", opacity: "70%" }
+                    : null
+                }
+              >
+                <img
+                  src={require("../assets/battle_image.PNG")}
+                  alt="1v1 Battle Selection"
+                />
+              </div>
+
+              <h3 align="center" className="image-para">
+                1v1 Battle
+              </h3>
             </div>
-            <h3 align="center" className="image-para">
-              1v1 Battle
-            </h3>
-          </div>
 
-          <div className="image-container">
-            <Link
-              onClick={() => {
-                window.appComponent.setState({
-                  notificationMessage:
-                    "Sorry, the shooting game is not ready yet!",
-                });
-              }}
-            >
-              <img
-                src={require("../assets/shooting_image.PNG")}
-                alt="Shooting Selection"
-              />
-            </Link>
-            <h3 align="center" className="image-para">
-              Shooting
-            </h3>
+            <div className="image-container">
+              <Link
+                onClick={() => {
+                  window.appComponent.setState({
+                    notificationMessage:
+                      "Sorry, the shooting game is not ready yet!",
+                  });
+                }}
+              >
+                <img
+                  src={require("../assets/shooting_image.PNG")}
+                  alt="Shooting Selection"
+                />
+              </Link>
+              <h3 align="center" className="image-para">
+                Shooting
+              </h3>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
