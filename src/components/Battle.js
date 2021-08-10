73765e60e82de0b14edb7e92fd7bf0f2cc83ac99
alvynben <alvynben@gmail.com>
2021-08-10 11:04:36 +0800

    Enable transfer from controller to next user

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,64 +1,96 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { withRouter, useLocation, useHistory } from "react-router-dom";
 
-import keyboardController from "../common/KeyboardController.js"
-import WaitingQueue from "./WaitingQueue.js"
+import keyboardController from "../common/KeyboardController.js";
+import WaitingQueue from "./WaitingQueue.js";
 
-import webRTC from '../api/WebRTC.js';
+import webRTC from "../api/WebRTC.js";
 
 import "./Battle.css";
 import "../common/Animations.css";
+import signallingServer from "../api/SignallingServer.js";
 
 function Battle(props) {
-    const history = useHistory();
-    const location = useLocation();
+  const history = useHistory();
+  const location = useLocation();
 
-    const [videoStream, setVideoStream] = useState(null);
-    const videoRef = useRef(null);
+  const [videoStream, setVideoStream] = useState(null);
+  const videoRef = useRef(null);
 
-    useEffect(() => {
-        if (!location.username) {
-            window.alert("Please log in before playing a game!");
-            history.push("/");
-        } else if (history.purpose === "playing") {
-            webRTC.setVideoCallback(setVideoStream);
-            keyboardController.setKeyPressCallback((pressedKeys) => {
-                webRTC.sendKeyPress(pressedKeys);
-            });
-            keyboardController.start();
+  useEffect(() => {
+    if (!location.username) {
+      window.alert("Please log in before playing a game!");
+      history.push("/");
+    } else if (location.purpose === "playing") {
+      console.log(location);
+      webRTC.setVideoCallback(setVideoStream);
+      keyboardController.setKeyPressCallback((pressedKeys) => {
+        webRTC.sendKeyPress(pressedKeys);
+      });
+      keyboardController.start();
+    } else if (location.purpose === "waiting") {
+      const myVar = setInterval(() => {
+        if (signallingServer.offerMessage) {
+          webRTC.initializePeerConnection().then(() => {
+            signallingServer
+              .sendOffer(
+                signallingServer.offerMessage.robotName,
+                webRTC.getOffer()
+              )
+              .then((answer) => {
+                console.log("Game can be started");
+                webRTC.setAnswer(answer);
+                signallingServer.startGame();
+
+                clearInterval(myVar)
+
+                history.push({
+                  pathname: "/game-select/battle",
+                  username: location.username,
+                  purpose: "playing",
+                });
+              });
+          });
         }
-    }, []);
+      }, 3000);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.srcObject = videoStream
-        }
-    }, [videoStream]);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = videoStream;
+    }
+  }, [videoStream]);
 
-    return (
-        <div className="window-container">
-            <div className="help-container">
-                <h3 align="center">How To Play</h3>
-                <hr />
-                <img src={require("../assets/battle_instructions.png")} alt="Gameplay Instructions" />
-            </div>
+  return (
+    <div className="window-container">
+      <div className="help-container">
+        <h3 align="center">How To Play</h3>
+        <hr />
+        <img
+          src={require("../assets/battle_instructions.png")}
+          alt="Gameplay Instructions"
+        />
+      </div>
 
-            <div className="game-container">
-                <h3 align="center">1v1 Battle</h3>
-                <hr />
-                <video
-                    id="localRobotFeed"
-                    autoPlay={true}
-                    playsInline={true}
-                    ref={videoRef}>
-                </video>
-                <span id="battleTimeTitle">Time Left: </span><span id="battleTime" className="seconds">-</span>
-            </div>
+      <div className="game-container">
+        <h3 align="center">1v1 Battle</h3>
+        <hr />
+        <video
+          id="localRobotFeed"
+          autoPlay={true}
+          playsInline={true}
+          ref={videoRef}
+        ></video>
+        <span id="battleTimeTitle">Time Left: </span>
+        <span id="battleTime" className="seconds">
+          -
+        </span>
+      </div>
 
-            <WaitingQueue></WaitingQueue>
-
-        </div>
-    );
+      <WaitingQueue></WaitingQueue>
+    </div>
+  );
 }
 
 export default withRouter(Battle);
